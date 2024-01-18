8ee955efdab9cebd41223aa514a372ee01cb21b8
javeria-farooq <gettingeducation6@gmail.com>
2024-01-18 18:07:08 +0500

    11 JS Assignment

---
+++ app.js
@@ -0,0 +1,59 @@
+// let javArray = [' Javeria ']
+
+
+// push method
+
+// javArray.push(' Farooq ')
+// javArray.push( 20 )
+// javArray.push(true)
+// javArray.push("Intermediate" , "A grade")
+// document.write(javArray + "<br>")
+
+// pop method
+
+// javArray.pop()
+// javArray.pop()
+// document.write(javArray + "<br>")
+// javArray.pop()
+// javArray.pop()
+// javArray.pop()
+// javArray.pop()
+// document.write(javArray + " <br>")
+
+
+
+// unshift method
+
+// javArray.unshift('programmer')
+// javArray.unshift('Miss')
+// javArray.unshift('Miss' + ' programmer')
+// javArray.unshift("Miss" , 'Programmer')
+// document.write(javArray)
+
+// shift method
+
+// javArray.shift()
+// javArray.shift()
+// javArray.shift()
+// document.write(javArray)
+
+
+// splice method
+
+// javArray.splice(1, 0, 'hi', 'hello world') 
+// javArray.splice(0, 0, 'web developer')
+// javArray.splice(1, 1, 'Web developer')
+// document.write(javArray)
+// var arra = ['javeria ', ' farooq ', 20 ,  true]
+// document.write(arra[0] + " " + arra[1])
+// document.write(arra + "<br>")
+
+// let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+// let arr2 = arr.slice(1, 6)
+// document.write(arr2)
+// slice method
+
+// let arr = [1,2,3,4,5,6,7,8]
+// let arr2 = arr.slice(0, 6)
+// document.write(arr2)
+// console.log(arr2)
